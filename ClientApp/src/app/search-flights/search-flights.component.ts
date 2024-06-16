ebb8b953459f663d7fcba7a647ce928cee32e415
majdhallak <majdhallak90@gmail.com>
2024-06-16 21:28:31 +0100

    RM

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -3,19 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
-  styleUrls: ['./search-flights.component.css']
+  styleUrls: ['./search-flights.component.css'],
 })
 export class SearchFlightsComponent implements OnInit {
-  
-  searchResults : any = [
-    "American Airlines", 
-    "British Airways",
-    "Lufthansa"
-  ]
-  constructor(){}
+  searchResult: FlightRm[] = [
+    {
+      airline: 'American Airlines',
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: 'Los Angeles' },
+      arrival: { time: Date.now().toString(), place: 'Istanbul' },
+      price: '350',
+    },
+    {
+      airline: 'Deutsche BA',
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: 'Munchen' },
+      arrival: { time: Date.now().toString(), place: 'Schiphol' },
+      price: '600',
+    },
+    {
+      airline: 'British Airways',
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: 'London, England' },
+      arrival: { time: Date.now().toString(), place: 'Vizzola-Ticino' },
+      price: '600',
+    },
+  ];
+
+  constructor() {}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
-  
+}
+export interface FlightRm {
+  airline: string; 
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
 
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
